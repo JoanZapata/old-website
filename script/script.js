a8984abfbc333522ff483020b18aae86430603e1
Joan Zapata <jzapata@excilys.com>
2012-02-07 23:18:25 +0100

    Fixed a few things

--- script/script.js
+++ script/script.js
@@ -59,9 +59,8 @@ $(function() {
 		feedloaded: function(){
 			count++;
 			if( count === list.length ){
-				$(".lastActivity").animate({left: '0px', opacity: '1'}, 'slow', 'easeOutQuad');
-			    
 				lifestreamDisplayed = true;
+				$(".lastActivity").animate({left: '0px', opacity: '1'}, 'slow', 'easeOutQuad');
 				$("#lifestream li").each(function(index){
 					var element = $(this),
 					date = new Date(element.data("time"));
@@ -79,15 +78,18 @@ $(function() {
 	});
 	
 	$(".link").each(function(index){
+		$(this).prepend('<img class="shadow" src="images/shadow.png" />');
 		$(this).css('right', $(window).width()/2+'px').css('opacity', '0')
-				.delay(6*300-index*300)
+				.delay(index*300)
 				.animate({right: '0px', opacity: '0.7'}, 'slow', 'easeOutQuad').animate({opacity: '1'});
 	});
 	
-	$(".link").hover(function(){
-		$(this).animate({bottom:'15px'}, "fast");
+	$("#links li").hover(function(){
+		$(this).find(".shadow").animate({width:'35px', left:'15px'}, "fast");
+		$(this).find("a img").animate({bottom:'15px'}, "fast");
 	}, function(){
-		$(this).animate({bottom:'0px'}, "fast");
+		$(this).find(".shadow").animate({width:'64px', left:'0px'}, "fast");
+		$(this).find("a img").animate({bottom:'0px'}, "fast");
 	});
 	
 });
